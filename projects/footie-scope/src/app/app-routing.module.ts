efda5d3297ec0d66d99a391d5b8a0161b899a066
Tarek Peregrina <debraye@gmail.com>
2020-11-02 19:23:23 +0100

    styling issues

--- projects/footie-scope/src/app/app-routing.module.ts
+++ projects/footie-scope/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path: '**', redirectTo: 'home'}
+  { path: '**', redirectTo: 'home' }
   ];
 
 @NgModule({
