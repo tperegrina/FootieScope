ac35120e9eb32e8844a76dd0c91b63490e98fac3
Tarek Peregrina <debraye@gmail.com>
2020-11-05 17:28:49 +0100

    created rank icon component

--- projects/footie-scope/src/app/pages/classic-league/rank-icon/rank-icon.component.ts
+++ projects/footie-scope/src/app/pages/classic-league/rank-icon/rank-icon.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+enum Rank {
+  UP,
+  DOWN,
+  NO_CHANGE
+}
 
 @Component({
   selector: 'footie-sc-rank-icon',
@@ -6,10 +12,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rank-icon.component.scss']
 })
 export class RankIconComponent implements OnInit {
+  @Input('current') current: number = 0;
+  @Input('previous') previous: number = 0;
+  public eRank = Rank;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  public isRankIncreased() {
+    if (this.current == this.previous) {
+      return Rank.NO_CHANGE;
+    } else if (this.current - this.previous < 0 ) {
+      return Rank.UP;
+    } else {
+      return Rank.DOWN;
+    }
   }
 
 }
