d74bb057e3771535d58bef41ca3c1e01100f26da
Tarek Peregrina <debraye@gmail.com>
2020-11-13 17:58:55 +0100

    added params to route for League Classic

--- projects/footie-scope/src/app/app-routing.module.ts
+++ projects/footie-scope/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'classic-league' },
-  { path: 'classic-league', loadChildren: () => import('./pages/classic-league/classic-league.module').then(m => m.ClassicLeagueModule) },
+  { path: 'classic-league/:leagueId', loadChildren: () => import('./pages/classic-league/classic-league.module').then(m => m.ClassicLeagueModule) },
   { path: '**', redirectTo: 'classic-league' }
   ];
 
